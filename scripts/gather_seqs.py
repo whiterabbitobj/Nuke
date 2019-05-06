bd457c746eaa49441eaf830dd625007a5db66cfc
Matthew Doll <mattdoll@gmail.com>
2019-05-06 13:18:06 -0500

    script is fully working. currently does not filter by filetype even if EXT is provided

--- scripts/gather_seqs.py
+++ scripts/gather_seqs.py
@@ -1,40 +1,49 @@
 import os
-# import os.path
 import pyseq
 import subprocess
 from argparse import ArgumentParser
 
-def get_seqs(path, ext=""):
-    dirs = [[root, subdirs] for root, subdirs, _ in os.walk(path)]
+
+# Recursively collect filsets below provided directory
+def get_seqs(path, ext="", concise=True):
     seq_list = []
-    for root, subdirs in dirs:
-        sets = pyseq.get_sequences(root + '/*' + ext)
-        if len(sets) == 1:
-            print(type(sets[0].))
-        if subdirs and not sets:
-            continue
-        print("\nFilesets found in {}:".format(root))
-        if  not sets:
-            print("EMPTY DIRECTORY!!!")
-        for s in sets:
-            print("---> {}".format(s))
-            seq_list.append(os.path.join(root,s.format('%h#%t')).replace('\\', '/'))
+    for root, dirs, seq in pyseq.walk(path):
+        root = root.replace('\\', '/')
+        if seq:
+            if not concise:
+                print("\nFilesets found in ({}):".format(root))
+            for s in seq:
+                if concise:
+                    print(s.path())
+                else:
+                    print("---> {}".format(s))
+                if len(s) > 1:
+                    s = s.format('%h#%t')
+                seq_list.append(os.path.join(root, str(s)))
+        elif not dirs:
+            print("\n***WARNING***")
+            print("FOUND AN EMPTY BOTTOM-LEVEL DIRECTORY! ({})\n".format(root))
     return seq_list
 
 
+# Collect command line arguments
 parser = ArgumentParser()
 parser.add_argument("path", nargs='?', type=str, default=os.getcwd())
 parser.add_argument("-ext", type=str)
 parser.add_argument("-rv", action="store_true")
+parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
 path = args.path
 ext = args.ext
 do_rv = args.rv
+concise = not args.verbose
 
 
+# Work with the collected sequences
 print("Collecting image sequences from: {}".format(path))
 
-
-seqs = get_seqs(path)
+seqs = get_seqs(path, ext, concise)
+print("\n\n...found {} sequences.".format(len(seqs)))
 if do_rv:
-    subprocess.Popen('E:/Software/rv-win64-x86-64-6.2.3/bin/rv.exe {}'.format(' '.join(get_seqs(path))))
+    print("Opening RV...")
+    subprocess.Popen('E:/Software/rv-win64-x86-64-6.2.3/bin/rv.exe {}'.format(' '.join(seqs)))
